0535ec98d6db90ca035605a268b68243782b1877
Ben Dill <ben@dillfamily.org>
2015-11-15 12:09:00 -0500

    Adding untracked files

---
+++ SentimentAnalysis.Core/Domain/Result.cs
@@ -0,0 +1,42 @@
+﻿namespace SentimentAnalysis.Core.Domain
+{
+    public class Result
+    {
+        private Result(bool success, string error, Sentiment sentiment)
+        {
+            Success = success;
+            Error = error;
+            Sentiment = sentiment;
+        }
+
+        public bool Success { get; }
+
+        public string Error { get; }
+
+        public Sentiment Sentiment { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var r = (Result)obj;
+
+            return Success == r.Success && Sentiment == r.Sentiment && Error == r.Error;
+        }
+
+        public override int GetHashCode() => Sentiment.GetHashCode() ^ Success.GetHashCode() ^ Error?.GetHashCode() ?? 0;
+
+        public static Result Build(Sentiment sentiment)
+        {
+            return new Result(true, null, sentiment);
+        }
+
+        public static Result Build(string error)
+        {
+            return new Result(false, error, Sentiment.Invalid);
+        }
+    }
+}
