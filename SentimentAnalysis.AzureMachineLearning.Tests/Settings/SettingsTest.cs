75313e36641862cf09edb2ab337c4a89e6edc43f
Ben Dill <ben@dillfamily.org>
2015-11-18 09:17:04 -0500

    untracked file

---
+++ SentimentAnalysis.AzureMachineLearning.Tests/Settings/SettingsTest.cs
@@ -0,0 +1,18 @@
+﻿namespace SentimentAnalysis.AzureMachineLearning.Tests.Settings
+{
+    using DotNetTestHelper;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AzureMachineLearningSettings = AzureMachineLearning.AzureMachineLearningSettings;
+
+    [TestClass]
+    public abstract class SettingsTest
+    {
+        protected AzureMachineLearningSettings Sut { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            Sut = new SutBuilder<AzureMachineLearningSettings>().Build();
+        }
+    }
+}
